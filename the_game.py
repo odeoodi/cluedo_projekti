1cbf4b2e37329fb83c22f3197e69a511eedaeaaa
odeoodi <ode.ojala@metropolia.fi>
2024-09-27 08:48:09 +0300

    päivitetty

--- the_game.py
+++ the_game.py
@@ -3,25 +3,62 @@ from tabnanny import check
 import mysql.connector
 import random
 
-def check_money(game_id):
-    sql = f'select money from game where id = "{game_id}"'
+def check_money(saved_game):
+    sql = f'select money from game where id = "{saved_game}"'
     cursor = db_connection.cursor()
     cursor.execute(sql)
     money = cursor.fetchone()
     money_now = money[0]
     return money_now
 
-def win():
+#def accuse_weapon():
+    #adds the accused weapon to accusations table
+
+
+def correct_weapon():
+    # tarvitsen right_answers taulusta oikeat vastaukset ja accusations taulusta syytökset. Vertaan, onko kaikki right_answers
+    # taulun tiedot accusations taulussa. Periaatteessa toimii, jos yhdistän taulut ja katson onko right_answers.id_weapons
+    # (ja sama kuin weapons -taulun id, josta saadaan weaponin nimi) myös accusations taulussa ja sama locationille ja suspecteille.
+    sql = f'from right_answers inner join goal_reached on goal_reached.right_answers_id = right_answers.id_weapons inner join weapons on weapons.id = right_answers.id_weapons where right_answers.id_weapons = weapons.id'
+    cursor = db_connection.cursor()
+    cursor.execute(sql)
+    right_weapon = cursor.fetchall()
+    if len(right_weapon) == 1:
+        first_right = True
+    return first_right
+
+def correct_suspect():
     #tarvitsen right_answers taulusta oikeat vastaukset ja accusations taulusta syytökset. Vertaan, onko kaikki right_answers
     # taulun tiedot accusations taulussa. Periaatteessa toimii, jos yhdistän taulut ja katson onko right_answers.id_weapons
     # (ja sama kuin weapons -taulun id, josta saadaan weaponin nimi) myös accusations taulussa ja sama locationille ja suspecteille.
     sql = f''
     cursor = db_connection.cursor()
     cursor.execute(sql)
-    game_won = cursor.fetchall()
-    if game_won:
+    right_suspect = cursor.fetchall()
+    if right_suspect:
+        second_right = True
+    return second_right
+
+def correct_location():
+    #tarvitsen right_answers taulusta oikeat vastaukset ja accusations taulusta syytökset. Vertaan, onko kaikki right_answers
+    # taulun tiedot accusations taulussa. Periaatteessa toimii, jos yhdistän taulut ja katson onko right_answers.id_weapons
+    # (ja sama kuin weapons -taulun id, josta saadaan weaponin nimi) myös accusations taulussa ja sama locationille ja suspecteille.
+    sql = f''
+    cursor = db_connection.cursor()
+    cursor.execute(sql)
+    right_location = cursor.fetchall()
+    if right_location:
+        third_right = True
+    return third_right
+
+def win():
+    loc_true = correct_location()
+    sus_true = correct_suspect()
+    wea_true = correct_weapon()
+    if loc_true and sus_true and wea_true:
         victory = True
     return victory
+
 
 
 
@@ -34,7 +71,7 @@ pseudo code for one round:
 intro() 
 
 
-while money > 0 or right_answers in accusations:
+while money > 0 or not right_answers in accusations:
     print(check_money())
     
     round = input("What would you like to do: ")
@@ -43,7 +80,9 @@ while money > 0 or right_answers in accusations:
     if round == "help":
         print(help())
     elif round == "accuse":
-        accusation = input("Make your accusations: ")
+        weapon_accusation = input("Make your weapon accusations: ")
+        suspect_accusation = input("Make your suspect accusations: ")
+        location_accusation = location_now()
         print(check_if_correct())
     elif round == "fly":
         destination = input("Where would you like to travel: ")
@@ -53,7 +92,7 @@ while money > 0 or right_answers in accusations:
     elif round == "end game":
         end_game()
     
-if money == 0:
+if money <= 0:
     print("you lost the game")
 else:
     print("you won")
@@ -71,7 +110,34 @@ db_connection = mysql.connector.connect(
 
 #intro()
 
+victory = False
 
 while check_money(1) > 0 and not victory:
     #saved_game = input("Select saved game: ") // possible if we want to save games to the game table and identify them by id number.
     print(f"You have {check_money(1)}€ left.")
+    print("")
+    print('See the options by typing "help".')
+    game_round = input("What would you like to do: ")
+
+    if game_round.lower() == "accuse":
+        weapon_accusation = input("Make your weapon accusation: ")
+        suspect_accusation = input("Who do you suspect: ")
+        location_accusation = location_now()
+        #print(check_if_correct())
+    elif game_round.lower() == "fly":
+        destination = input("Where would you like to fly next: ")
+        #fly()
+    elif game_round.lower() == "check accusations":
+        #print(accusations())
+    #elif game_round == "help":
+        #print(help())
+    #else:
+        print("Check spelling on your command.")
+
+#These can be changed to work better with the outro.
+if check_money(1) <= 0:
+    print("You ran out of money.")
+elif victory:
+    print("You solved the mystery!")
+
+
