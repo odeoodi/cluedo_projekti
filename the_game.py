60e1ba8d46fd23096d4b4e530faa273ef67616b0
odeoodi <ode.ojala@metropolia.fi>
2024-09-30 17:20:24 +0300

    vähän päivityksiä mun koodiin

--- the_game.py
+++ the_game.py
@@ -2,7 +2,15 @@ from tabnanny import check
 
 import mysql.connector
 import random
-
+def location():
+    # Empties locations tabel. Selects 5 random location from airport tabel and adds them to locations table.
+    sql1= (f'DELETE FROM LOCATIONS;')
+    sql2= (f'insert into locations(name) SELECT name FROM airport WHERE continent = "EU" and type = "large_airport" ORDER BY RAND() limit 5;')
+    cursor = db_connection.cursor()
+    cursor.execute(sql1)
+    cursor.execute(sql2)
+    db_connection.commit()
+    return
 def check_money(saved_game):
     sql = f'select money from game where id = "{saved_game}"'
     cursor = db_connection.cursor()
@@ -11,41 +19,33 @@ def check_money(saved_game):
     money_now = money[0]
     return money_now
 
-def accuse_weapon():
+def accuse_weapon_suspect():
     #adds the accused weapon to accusations table
     weapon_accusation = input("Make your weapon accusation: ")
-    sql = f'insert into accusations(weapon_accusations) values("{weapon_accusation}")'
-    cursor = db_connection.cursor()
-    cursor.execute(sql)
-    #fff = cursor.fetchone()
-    return
-
-def accuse_suspect():
     suspect_accusation = input("Who do you suspect: ")
-    sql = f'insert into accusations(suspect_accusations) values("{suspect_accusation}")'
+    #suspect_airport = location_now()
+    suspect_airport = "Belgium"
+    sql = f'insert into accusations(weapon_accusations,airport_accusations,suspect_accusations) values("{weapon_accusation}","{suspect_airport}","{suspect_accusation}")'
     cursor = db_connection.cursor()
     cursor.execute(sql)
     #fff = cursor.fetchone()
     return
-'''
-def accuse_location():
-    airport_accusation = location_now()
-    sql = f'insert into accusations(airport_accusations) values("{airport_accusation}")'
-    cursor = db_connection.cursor()
-    cursor.execute(sql)
-    #fff = cursor.fetchone()
-    return
-'''
 
+def check_accusations():
+    sql = f'select weapon_accusations,suspect_accusations,airport_accusations from accusations'
+    cursor = db_connection.cursor()
+    cursor.execute(sql)
+    made_accusations = cursor.fetchall()
+    for acc in made_accusations:
+        print(acc)
+    return made_accusations
 
 '''
 angelinan koodi check if correct
+
 def win():
-    loc_true = correct_location()
-    sus_true = correct_suspect()
-    wea_true = correct_weapon()
-    if loc_true and sus_true and wea_true:
-        victory = True
+    if check_if_correct has all three correct -> 
+    victory = True
     return victory
 '''
 
@@ -100,6 +100,7 @@ db_connection = mysql.connector.connect(
 #intro()
 
 victory = False
+#accusation_counter = 0
 
 while check_money(1) > 0 and not victory:
     #saved_game = input("Select saved game: ") // possible if we want to save games to the game table and identify them by id number.
@@ -107,21 +108,31 @@ while check_money(1) > 0 and not victory:
     print("")
     print('See the options by typing "help".')
     game_round = input("What would you like to do: ")
+    command_counter = 0
 
-    if game_round.lower() == "accuse":
-        accuse_weapon()
-        accuse_suspect()
-        #accuse_location()
-        #print(check_if_correct())
-    elif game_round.lower() == "fly":
+    while command_counter == 0:
+        if game_round.lower() == "accuse":
+            accuse_weapon_suspect()
+            #accusation_counter += 1
+            #accuse_suspect()
+            #accuse_location()
+            #print(check_if_correct())
+            command_counter += 1
+        elif game_round.lower() == "fly":
+            destination = input("Where would you like to fly next: ")
+            command_counter = 0
+            #fly()
+        elif game_round.lower() == "check accusations":
+            check_accusations()
+        elif game_round == "help":
+            print("hello")
+            #print(help_ville())
+        else:
+            print("Check spelling on your command.")
+    if command_counter == 1:
         destination = input("Where would you like to fly next: ")
-        #fly()
-    elif game_round.lower() == "check accusations":
-        #print(accusations())
-    #elif game_round == "help":
-        #print(help())
-    #else:
-        print("Check spelling on your command.")
+        command_counter = 0
+        # fly()
 
 #These can be changed to work better with the outro.
 if check_money(1) <= 0:
