6ed8bc7f1c2f5da3e800dc98cdccb3d3acdb57a1
Roskapanda69 <markku.pyrro@metropolia.com>
2024-10-01 15:18:05 +0300

    Jännittävää

--- make_codes.py
+++ make_codes.py
@@ -12,44 +12,40 @@ db_connection = mysql.connector.connect(
          autocommit=True
          )
 
-
+'''
 def start_location():
     # Empties locations tabel. Selects 5 random location from airport tabel and adds them to locations table,
     # selects one of the airports as starting airport.
     sql = (f'DELETE FROM game;')
+    sql_1 = (f'ALTER TABLE game AUTO_INCREMENT= 1;')
     sql1= (f'DELETE FROM LOCATIONS;')
+    sql1_1 = (f'ALTER TABLE locations AUTO_INCREMENT= 1;')
     sql2= (f'insert into locations(name, icao) SELECT name, ident FROM airport WHERE continent = "EU" and type = "large_airport" ORDER BY RAND() limit 5;')
     sql3= (f'insert into game(location) SELECT name FROM locations ORDER BY RAND() limit 1;')
     cursor = db_connection.cursor()
     cursor.execute(sql)
+    cursor.execute(sql_1)
     cursor.execute(sql1)
+    cursor.execute(sql1_1)
     cursor.execute(sql2)
     cursor.execute(sql3)
     db_connection.commit()
     return
 
-start_location()
 
 
-'''
-def start_location():
-    # select one random loaction from locations tabel as location where the game starts
-    sql= (f'insert into game(location) SELECT name FROM locations ORDER BY RAND() limit 1;')
-    cursor = db_connection.cursor()
-    cursor.execute(sql)
-    db_connection.commit()
-    return
-
-# def fly():
 
 start_location()
 
+
 '''
 
 
-'''
+
+
+
 def locations():
-    sql = (f'SELECT icao, name FROM locations;')
+    sql = (f'SELECT icao, name FROM locations WHERE name NOT IN ;')
     cursor = db_connection.cursor()
     cursor.execute(sql)
     airports = cursor.fetchall()
@@ -59,7 +55,7 @@ def locations():
 
 
 locations()
-
+'''
 def is_location_current(destination):
     sql = (f'SELECT icao, FROM locations;')
     sql2 = (f'SELECT icao, FROM game;')
