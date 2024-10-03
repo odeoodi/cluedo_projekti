3d9cbab9f3d79178a8d4141a67a3407f388fd829
odeoodi <ode.ojala@metropolia.fi>
2024-10-03 14:06:51 +0300

    vähän päivityksiä mun koodiin

--- oodi_koodaa.py
+++ oodi_koodaa.py
@@ -3,47 +3,11 @@ from tabnanny import check
 import mysql.connector
 import random
 
-def random_location(locations):
-    sql = f"SELECT name FROM locations ORDER BY RAND() LIMIT 1";
-    kursori=db_connection.cursor()
-    kursori.execute(sql)
-    result=kursori.fetchone()
-    return result
-
-def random_weapon(weapons):
-    sql = f"SELECT weapon FROM weapons ORDER BY RAND() LIMIT 1";
-    kursori=db_connection.cursor()
-    kursori.execute(sql)
-    result=kursori.fetchone()
-    return result
-
-def random_suspect(suspects):
-    sql = f"SELECT names FROM suspects ORDER BY RAND() LIMIT 1";
-    kursori=db_connection.cursor()
-    kursori.execute(sql)
-    result=kursori.fetchone()
-    return result
-
-
-
-db_connection = mysql.connector.connect(
-    host="localhost",
-    port=3306,
-    database="detective_game2",
-    user="heikki",
-    password="pekka",
-    autocommit=True)
-
-right_location = random_location(db_connection)
-right_weapon = random_weapon(db_connection)
-right_suspect = random_suspect(db_connection)
-print(f"{right_location[0]}, {right_weapon[0]}, {right_suspect[0]}")
 
 def start_location():
     # Empties locations tabel. Selects 5 random location from airport tabel and adds them to locations table,
     # selects one of the airports as starting airport.
-    sql1 = (
-        f"UPDATE locations SET name = (SELECT name FROM airport WHERE continent = 'EU' AND type = 'large_airport' ORDER BY RAND()LIMIT 1);")
+    sql1 = (f"UPDATE locations SET name = (SELECT name FROM airport WHERE continent = 'EU' AND type = 'large_airport' ORDER BY RAND()LIMIT 1);")
     sql2 = (f"UPDATE locations SET icao = (SELECT ident FROM airport WHERE locations.name = airport.name LIMIT 1);")
     sql3 = (f"UPDATE game SET location = (SELECT name FROM locations ORDER BY RAND() limit 1);")
     cursor = db_connection.cursor()
@@ -53,15 +17,13 @@ def start_location():
     db_connection.commit()
     return
 
-
 def start_accusations():
     sql1 = f"delete from accusations"
-    # sql2 = (f"")
+    #sql2 = (f"")
     cursor = db_connection.cursor()
     cursor.execute(sql1)
     hints = cursor.fetchall()
     return hints
-
 
 def start_money(game_id):
     sql = (f'UPDATE game SET money = 500 WHERE id = "{game_id}"')
@@ -98,11 +60,13 @@ def accuse_weapon_suspect(game_id):
     print("Suspects to choose from: Make, Iida, Ode, Angelina, Ville")
     suspect_accusation = input("Who do you suspect: ")
     airport_accusation = location_now(game_id)
+    #testi airport_accusation = "Belgium"
     sql = f'insert into accusations(weapon_accusations,location_accusations,suspect_accusations) values("{weapon_accusation}","{airport_accusation}","{suspect_accusation}")'
     cursor = db_connection.cursor()
     cursor.execute(sql)
+    #fff = cursor.fetchone()
+    #check_accusations(game_id)
     return
-
 
 def check_accusations(game_id):
     sql = (f'select weapon_accusations,location_accusations,suspect_accusations from accusations')
@@ -119,49 +83,84 @@ def check_accusations(game_id):
 
 
 def fly():
-    # Player flys to new location.
+    # Druing this funktion player can fly to the new location.
+
     def locations_available():
-        sql = (
+        # This print all the airports and their ICAO-codes where player can fly.
+        # Execludin the location where player is currently
+        availabled = (
             f"SELECT icao, name FROM locations LEFT JOIN game ON locations.name = game.location WHERE game.location IS NULL;")
         cursor = db_connection.cursor()
-        cursor.execute(sql)
+        cursor.execute(availabled)
         airports = cursor.fetchall()
         for airport in airports:
-            print(f' - {airport[1]}, ICAO: {airport[0]}. ')
-        print("")
+            print(f'Icao: {airport[0]}, {airport[1]}. ')
         return
 
-    def location_check(destination):
-        sql = (f'SELECT location FROM game ;')
-        sql2 = (f'SELECT name FROM locations WHERE icao = "{destination}";')
+    def icao_in_locations(destination):
+        # Checks if the icao code is writen correctly.
+        check = (f'SELECT name FROM locations; ')
+        getname = (f'SELECT name FROM locations WHERE icao = "{destination}";')
         cursor = db_connection.cursor()
-        cursor.execute(sql)
-        location_game = cursor.fetchall()
-        cursor.execute(sql2)
-        locations = cursor.fetchall()
-        if location_game != locations:
+        cursor.execute(check)
+        locations_check = cursor.fetchall()
+        cursor.execute(getname)
+        locations_icao_name = cursor.fetchall()
+        if not locations_icao_name:
+            return False
+        elif locations_icao_name[0] in locations_check:
             return True
-        elif location_game == locations:
+
+    def location_check(destination):
+        # Checks if the destination where player wants to go is current location or not.
+        # Returns True or False.
+        getlocation = (f'SELECT location FROM game ;')
+        getname = (f'SELECT name FROM locations WHERE icao = "{destination}";')
+        cursor = db_connection.cursor()
+        cursor.execute(getlocation)
+        location_game = cursor.fetchall()
+        cursor.execute(getname)
+        locations_icao = cursor.fetchall()
+        if location_game != locations_icao:
+            return True
+        else:
             return False
 
     def flying_new_port(destination):
-        sql = (f'UPDATE game SET location = (SELECT name FROM locations WHERE icao = "{destination}");')
+        # Uppdates a new location to the database where player has flown.
+        flying = (f'UPDATE game SET location = (SELECT name FROM locations WHERE icao = "{destination}");')
         cursor = db_connection.cursor()
-        cursor.execute(sql)
+        cursor.execute(flying)
         db_connection.commit()
         return
 
-    print(f'You are currently at the {location_now(1)}.\n')
-    print(f'Available airports for you to fly are:')
-    print(locations_available())
-    print("")
-    destination = input("Where would you like to fly next, use the ICAO-code: ")
-    if location_check(destination) == True:
-        flying_new_port(destination)
-        print(f'Welcome to {location_now(1)}.')
-        print(f"You have {check_money(select_game)}€ left.\n")
-    elif location_check(destination) == False:
-        print("You cannot stay at the same airport. If you do party people will leave and case won't be solved.")
+    def cost_of_flying():
+        # Subtracts flying cost from the money player has.
+        # Change (SELECT money -'125'....) to change cost.
+        moneycost = (f'UPDATE game SET money =( SELECT money -125 FROM game ) ')
+        cursor = db_connection.cursor()
+        cursor.execute(moneycost)
+        db_connection.commit()
+        return
+
+    while True:
+        print(f'You are currently at the {location_now(1)}.')
+        print(f'Available airports for you to fly are:')
+        print(locations_available())
+        destination = input("Where would you like to fly next, use the Icao-code: ")
+        destination = destination.upper()
+        if icao_in_locations(destination) == True:
+            if location_check(destination) == True:
+                flying_new_port(destination)
+                cost_of_flying()
+                print(f'Welcome to {location_now(1)} you have {check_money(1)} euros.')
+
+                break
+            elif location_check(destination) == False:
+                print(
+                    "You cannot stay at the same airport. If you do party people will leave and case won't be solved.")
+        else:
+            print("Sorry your Icao-code was not in the list, please try again.")
 
 
 '''
@@ -214,7 +213,6 @@ db_connection = mysql.connector.connect(
     password='pekka',
     autocommit=True
 )
-
 # intro()
 
 victory = False
