b589fc6011b124660eb0cf90f1cc09ce8d1d12a0
iidaaald <iida.aaldijk@metropolia.fi>
2024-10-04 13:10:41 +0300

    valmis

--- intro_story.py
+++ intro_story.py
@@ -1,8 +1,7 @@
 import textwrap
 
 story = '''
-Olet yksityinen etsivä viettämässä lomaa parhaiden kaveriesi kanssa. Olette vuokranneet yksityisen lentokoneen, joka matkustaa ympäri Eurooppaa. Loman lopussa matkareitin viimeisessä maassa löydätte ruumiin piilotettuna lentokoneen ruumasta. Puet etsivähattusi takaisin päälle napataksesi ystäväsi murhaajan. Kansainvälinen poliisi ilmoittaa, että sinulla on rajallinen budjetti (500 €) ja matkustamiseen kuluu 50 €. Sinun täytyy saada oikeutta ystävällesi saamalla vastuullinen kaltereiden taakse. Sinun täytyy kerätä vihjeitä rikoksesta ja niiden perusteella tehdä syytöksiä murhaajasta, murha- aseesta sekä maasta, missä murha tapahtui. 
-
+You are a private detective on vacation with your closest friends. You’ve rented a private plane that’s been flying all around Europe. At the end of your trip, in the final country on your itinerary, you discover a body hidden in the cargo hold of the plane. Without hesitation, you put your detective hat back on, determined to catch your friend's killer. Interpol informs you that you have a limited budget of 500 €, and each trip costs 50 €. You must seek justice for your friend by bringing the culprit behind bars. To do so, you’ll need to gather clues about the crime and use them to make accusations about the murderer, the murder weapon, and the country where the crime took place.
 '''
 
 wrapper = textwrap.TextWrapper(width=80, break_long_words=False, replace_whitespace=False)
