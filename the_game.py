c466f02a8e88e932b540242af1e34f80edef8a03
Roskapanda69 <markku.pyrro@metropolia.com>
2024-11-27 15:03:14 +0200

    Lulle

--- the_game.py
+++ the_game.py
@@ -19,49 +19,6 @@ def press_enter_to_continue():
 
 
 
-def check_if_correct_weapon(weapon_accusation):
-    # Checks whether the weapon accusation is correct.
-    sql1 = f"SELECT id FROM weapons WHERE weapon = '{weapon_accusation}' "
-    kursori = db_connection.cursor()
-    kursori.execute(sql1)
-    accusation_id = kursori.fetchone()[0]
-    sql2 = f"SELECT id_weapons FROM right_answers WHERE id_weapons = '{accusation_id}' "
-    kursori.execute(sql2)
-    accusations = kursori.fetchone()
-    matches = []
-    if not accusations:
-        return False
-    elif accusations:
-        return True
-
-def check_if_correct_location(airport_accusation):
-    # Checks whether the location accusation is correct.
-    sql1 = f"SELECT id FROM locations WHERE name = '{location_now(1)}' "
-    kursori = db_connection.cursor()
-    kursori.execute(sql1)
-    accusation_id = kursori.fetchone()[0]
-    sql2 = f"SELECT id_locations FROM right_answers WHERE id_locations = '{accusation_id}' "
-    kursori.execute(sql2)
-    accusations = kursori.fetchone()
-    if not accusations:
-        return False
-    elif accusations:
-        return True
-
-def check_if_correct_suspect(suspect_accusation):
-    # Checks whether the suspect accusation is correct.
-    sql1 = f"SELECT id FROM suspects WHERE names = '{suspect_accusation}' "
-    kursori = db_connection.cursor()
-    kursori.execute(sql1)
-    accusation_id = kursori.fetchone()[0]
-    sql2 = f"SELECT id_suspects FROM right_answers WHERE id_suspects = {accusation_id} "
-    kursori.execute(sql2)
-    accusations = kursori.fetchone()
-    matches = []
-    if not accusations:
-        return False
-    elif accusations:
-        return True
 
 def accuse_weapon_suspect(game_id, the_accusation):
     # --- adds the accused weapon to accusations table
