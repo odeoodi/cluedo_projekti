dad077efc9c61c90b36167a177eccf701aa201e6
Roskapanda69 <markku.pyrro@metropolia.com>
2024-12-03 22:04:52 +0200

    muutettu koodi kauniimmaksi

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 import mysql.connector
 import codes.config
 from codes.start import start_money, start_location, start_accusations, insert_right_answers
+from codes.get_from_sql import from_sql_weapons, form_sql_suspects,from_sql_locations
 from codes.check_if_correct import check_if_correct_location, check_if_correct_weapon, check_if_correct_suspect
 
 from codes.fly import flying_new_port, cost_of_flying
@@ -31,6 +32,25 @@ def new_game():
     insert_right_answers()
     return 'ok'
 
+@app.route('/getweapons')
+def weapons_data():
+    data = from_sql_weapons()
+    jsondata = json.dumps(data)
+    return jsondata
+
+@app.route('/getsuspects')
+def suspects_data():
+    data = form_sql_suspects()
+    jsondata = json.dumps(data)
+    return jsondata
+
+@app.route('/getlocations')
+def locations_data():
+    data = from_sql_locations()
+    jsondata = json.dumps(data)
+    return jsondata
+
+
 @app.route('/fly/<icao>')
 def in_game_fly(icao):
     icao = (icao)
@@ -48,6 +68,8 @@ def accuse(weapon, suspect, location):
     is_location = check_if_correct_location(location)
     jsonanwsver = [is_weapon, is_suspect, is_location]
     return jsonanwsver
+
+
 
 
 
--- codes/get_from_sql.py
+++ codes/get_from_sql.py
@@ -10,19 +10,23 @@ db_connection = mysql.connector.connect(
 )
 
 
-def from_sql():
+def from_sql_weapons():
     get_info = f"SELECT weapon, attribute1, attribute2  FROM weapons"
-    get_info2 = f"SELECT names, sex, age, glasses  FROM suspects"
-    get_info3 = f"SELECT name, icao  FROM locations"
     kursori = db_connection.cursor()
     kursori.execute(get_info)
     get_weapons = kursori.fetchall()
+    return get_weapons
+
+def form_sql_suspects():
+    get_info2 = f"SELECT names, sex, age, glasses  FROM suspects"
+    kursori = db_connection.cursor()
     kursori.execute(get_info2)
     get_suspects = kursori.fetchall()
+    return get_suspects
+
+def from_sql_locations():
+    get_info3 = f"SELECT name, icao, latitude_deg,longitude_deg   FROM locations"
+    kursori = db_connection.cursor()
     kursori.execute(get_info3)
     get_locations = kursori.fetchall()
-    print(get_weapons)
-    print(get_suspects)
-    print(get_locations)
-
-from_sql()
+    return get_locations
