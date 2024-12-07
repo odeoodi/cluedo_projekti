e3b1c54189cdcdb78933c4dd1453eeb327a7f328
odeoodi <ode.ojala@metropolia.fi>
2024-12-07 14:43:24 +0200

    notepad hint list tyhjenee uuden pelin alussa

--- html/javascript/map.js
+++ html/javascript/map.js
@@ -1,8 +1,17 @@
+'use strict'
+// VARIABLES
 let lat = 51.505 // here we need to add the longitude and latitude from get_location
 let long = -0.09
-
-
 const map = L.map('map').setView([lat, long], 5);
+let mapLocations = {}
+const url_py = 'http://127.0.0.1:3000'
+const new_game = document.getElementById('newgame-button')
+
+// FUNCTIONS
+new_game.addEventListener('click', () => {
+
+})
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
