98c5d0164a479a6cd8886b4cba53ca4fdb4ce0f5
odeoodi <ode.ojala@metropolia.fi>
2024-12-09 12:39:16 +0200

    kartan pop up toimii

--- html/javascript/map.js
+++ html/javascript/map.js
@@ -5,7 +5,9 @@ const map = L.map('map').setView([48.499998, 23.3833318 ], 3.4);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
+
 const url_py = 'http://127.0.0.1:3000'
+
 
 // FUNCTIONS
 
@@ -16,18 +18,81 @@ let long1, long2, long3, long4, long5, long6, long7
 [lat1, lat2, lat3, lat4, lat5, lat6, lat7] = locations_list.map(loc => loc[2]);
 [long1, long2, long3, long4, long5, long6, long7] = locations_list.map(loc => loc[3]);
 
+const flagImgHTML = `<img src="${locations_list[7][0][1]}" alt="Country Flag" style="width:30px;height:auto;">`;
 
-console.log(lat1,long1)
-    const marker1 = L.marker([lat1, long1]).addTo(map)
-    .bindPopup('Pop-up') //adds the pop-up, here we can add the airport info
+const marker1 = L.marker([lat1, long1]).addTo(map)
+    .bindPopup(`
+            <div id="map-popup">
+                <p><strong>Airport:</strong> ${locations_list[0][0]}</p>
+                <p><strong>Country:</strong> ${locations_list[7][0][2]}</p>
+                <p><strong>City:</strong> ${locations_list[7][0][0]}</p>
+                <p><strong>Flag:</strong></p>
+                <img src="${locations_list[7][0][1]}" alt="Country Flag"
+                       style="width:30px;height:auto;">
+</div>
+    `)
     .openPopup() // opens the pop-up without the marker being clicked, when the marker is created
 
   const marker2 = L.marker([lat2, long2]).addTo(map).bindPopup()
+    .bindPopup(`
+            <div id="map-popup">
+                <p><strong>Airport:</strong> ${locations_list[0][1]}</p>
+                <p><strong>Country:</strong> ${locations_list[7][1][2]}</p>
+                <p><strong>City:</strong> ${locations_list[7][1][0]}</p>
+                <p><strong>Flag:</strong></p>
+                <img src="${locations_list[7][1][1]}" alt="Country Flag" style="width:30px;height:auto;">
+            </div>
+        `)
   const marker3 = L.marker([lat3, long3]).addTo(map).bindPopup()
+    .bindPopup(`
+            <div id="map-popup">
+                <p><strong>Airport:</strong> ${locations_list[0][2]}</p>
+                <p><strong>Country:</strong> ${locations_list[7][2][2]}</p>
+                <p><strong>City:</strong> ${locations_list[7][2][0]}</p>
+                <p><strong>Flag:</strong></p>
+                <img src="${locations_list[7][2][1]}" alt="Country Flag" style="width:30px;height:auto;">
+            </div>
+        `)
   const marker4 = L.marker([lat4, long4]).addTo(map).bindPopup()
+    .bindPopup(`
+              <div id="map-popup">
+                  <p><strong>Airport:</strong> ${locations_list[0][3]}</p>
+                  <p><strong>Country:</strong> ${locations_list[7][3][2]}</p>
+                  <p><strong>City:</strong> ${locations_list[7][3][0]}</p>
+                  <p><strong>Flag:</strong></p>
+                  <img src="${locations_list[7][3][1]}" alt="Country Flag" style="width:30px;height:auto;">
+              </div>
+          `)
   const marker5 = L.marker([lat5, long5]).addTo(map).bindPopup()
+      .bindPopup(`
+              <div id="map-popup">
+                  <p><strong>Airport:</strong> ${locations_list[0][4]}</p>
+                  <p><strong>Country:</strong> ${locations_list[7][4][2]}</p>
+                  <p><strong>City:</strong> ${locations_list[7][4][0]}</p>
+                  <p><strong>Flag:</strong></p>
+                  <img src="${locations_list[7][4][1]}" alt="Country Flag" style="width:30px;height:auto;">
+              </div>
+          `)
   const marker6 = L.marker([lat6, long6]).addTo(map).bindPopup()
+      .bindPopup(`
+              <div id="map-popup">
+                  <p><strong>Airport:</strong> ${locations_list[0][5]}</p>
+                  <p><strong>Country:</strong> ${locations_list[7][5][2]}</p>
+                  <p><strong>City:</strong> ${locations_list[7][5][0]}</p>
+                  <p><strong>Flag:</strong></p>
+                  <img src="${locations_list[7][5][1]}" alt="Country Flag" style="width:30px;height:auto;">
+              </div>
+          `)
   const marker7 = L.marker([lat7, long7]).addTo(map).bindPopup()
+      .bindPopup(`
+              <div id="map-popup">
+                  <p><strong>Airport:</strong> ${locations_list[0][6]}</p>
+                  <p><strong>Country:</strong> ${locations_list[7][6][2]}</p>
+                  <p><strong>City:</strong> ${locations_list[7][6][0]}</p>
+                  <p><strong>Flag:</strong></p>
+                  <img src="${locations_list[7][6][1]}" alt="Country Flag" style="width:30px;height:auto;">
+              </div>
+          `)
 
   marker1.on('click', () => {
   console.log(`marker 1 in ${locations_list[7][0][0]} clicked`)});
@@ -43,6 +108,14 @@ console.log(lat1,long1)
   console.log(`marker 6 in ${locations_list[7][5][0]} clicked`)});
   marker7.on('click', () => {
   console.log(`marker 7 in ${locations_list[7][6][0]} clicked`)});
+
+
+  let clicked = ''
+  function onMapClick(e) {
+          clicked = "You clicked the map at " + e.latlng.toString()
+          console.log(clicked)
+}
+map.on('click', onMapClick);
 }
 
 function DeleteMap() {
