c9f3312883d61a03cfd2295de5a84b47a838fbf5
Roskapanda69 <markku.pyrro@metropolia.com>
2024-12-11 16:40:19 +0200

    load toimiii

--- codes/game_saves.py
+++ codes/game_saves.py
@@ -12,13 +12,17 @@ def load_game(game_id, connection):
     connect = connection
     note_load = f'SELECT content FROM notepab_text WHERE id = {game_id}'
     narrator_load = f'SELECT content FROM narrator_text WHERE id = {game_id}'
+    name_load = f'SELECT name FROM game WHERE id = {game_id}'
     cursor = connect.cursor()
     cursor.execute(note_load)
     note_text = cursor.fetchall()
     cursor.execute(narrator_load)
     narrator_text = cursor.fetchall()
+    cursor.execute(name_load)
+    name_load = cursor.fetchall()
     data_to_send = {
         'note_text': note_text,
         'narrator_text': narrator_text,
+        'player_name': name_load,
     }
     return data_to_send
