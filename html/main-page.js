98e43792b0a977beedc59ff0a0bda987421d7f97
angel <angelipr@metropolia.fi>
2024-12-11 16:30:55 +0200

    jeejee

--- html/main-page.js
+++ html/main-page.js
@@ -241,7 +241,7 @@ function first_start() {
     }}
 // let's use it when we need it: first_start()
 
-async function accuse() {
+async function accuse(weapon,suspect) {
     try {
         const response = await fetch( `${url_py}/accuse/${weapon}/${suspect}/${location_game}`)
         if (!response.ok) throw new Error("something went wrong")
@@ -276,6 +276,126 @@ fly_button.addEventListener('click', async () => {
     overlay.style.display = 'block'
     await createIcaoButtons() // also adds an event listener for icao buttons, where we can use the fly function
 })
+
+
+
+const odeButton = document.getElementById('ode')
+const iidaButton = document.getElementById('iida')
+const angelinaButton = document.getElementById('angelina')
+const villeButton = document.getElementById('ville')
+const makeButton = document.getElementById('make')
+const roopeButton = document.getElementById('roope')
+const oskariButton = document.getElementById('oskari')
+const lucaButton = document.getElementById('luca')
+const neaButton = document.getElementById('nea')
+const emmetButton = document.getElementById('emmet')
+const moriartyButton = document.getElementById('moriarty')
+const ghostButton = document.getElementById('the-Ghost-of-the-Late-Queen-of-England')
+
+
+
+
+const poisonButton = document.getElementById('poison')
+const ropeButton = document.getElementById('rope')
+const pistolButton = document.getElementById('pistol')
+const glassAngelButton = document.getElementById('glassAngel')
+const glassShardButton = document.getElementById('glassShard')
+const drowningButton = document.getElementById('drowning')
+const pushedDownButton = document.getElementById('pushedDown')
+const fountainPenButton = document.getElementById('fountainPen')
+const spoonButton = document.getElementById('spoon')
+const brokenGlassBottleButton = document.getElementById('brokenGlassBottleButton')
+const glassTrophyButton = document.getElementById('glassTrophy')
+const strawButton = document.getElementById('straw')
+const hammerButton = document.getElementById('hammer')
+const knifeButton = document.getElementById('knife')
+const plasticBagButton = document.getElementById('plasticBag')
+
+const idList = ["suspect","weapon"]
+
+async function ButtonChooserSuspect(button) {
+    idList[0]=button.id
+    console.log("Suspect selected:", idList[0]);
+}
+
+async function ButtonChooserWeapon(button) {
+    idList[1]=button.id
+    console.log("weapon selected:", idList[1]);
+}
+
+async function accuser() {
+    const idListsend = idList
+    let dataToSend = {
+        suspect: idListsend[0],  // Suspect selected
+        weapon: idListsend[1]    // Weapon selected
+    };
+
+    try {
+        const response = await fetch(`${url_py}/accuse/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataToSend)
+        });
+
+        if (!response.ok) throw new Error("Something went wrong while saving data.");
+        const result = await response.json()
+        console.log(result); // For debugging
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+
+document.getElementById('accuse-button').addEventListener('click', () => accuser())
+odeButton.addEventListener('click', () => ButtonChooserSuspect(odeButton))
+iidaButton.addEventListener('click', () => ButtonChooserSuspect(iidaButton))
+angelinaButton.addEventListener('click', () => ButtonChooserSuspect(angelinaButton))
+villeButton.addEventListener('click', () => ButtonChooserSuspect(villeButton))
+makeButton.addEventListener('click', () => ButtonChooserSuspect(makeButton))
+roopeButton.addEventListener('click', () => ButtonChooserSuspect(roopeButton))
+emmetButton.addEventListener('click', () => ButtonChooserSuspect(emmetButton))
+lucaButton.addEventListener('click', () => ButtonChooserSuspect(lucaButton))
+neaButton.addEventListener('click', () => ButtonChooserSuspect(neaButton))
+oskariButton.addEventListener('click', () => ButtonChooserSuspect(oskariButton))
+moriartyButton.addEventListener('click', () => ButtonChooserSuspect(moriartyButton))
+ghostButton.addEventListener('click', () => ButtonChooserSuspect(ghostButton))
+
+fountainPenButton.addEventListener('click', () => ButtonChooserWeapon(fountainPenButton))
+knifeButton.addEventListener('click', () => ButtonChooserWeapon(knifeButton))
+pistolButton.addEventListener('click', () => ButtonChooserWeapon(pistolButton))
+spoonButton.addEventListener('click', () => ButtonChooserWeapon(spoonButton))
+poisonButton.addEventListener('click', () => ButtonChooserWeapon(poisonButton))
+plasticBagButton.addEventListener('click', () => ButtonChooserWeapon(plasticBagButton))
+hammerButton.addEventListener('click', () => ButtonChooserWeapon(hammerButton))
+strawButton.addEventListener('click', () => ButtonChooserWeapon(strawButton))
+brokenGlassBottleButton.addEventListener('click', () => ButtonChooserWeapon(brokenGlassBottleButton))
+glassAngelButton.addEventListener('click', () => ButtonChooserWeapon(glassAngelButton))
+glassTrophyButton.addEventListener('click', () => ButtonChooserWeapon(glassTrophyButton))
+drowningButton.addEventListener('click', () => ButtonChooserWeapon(drowningButton))
+ropeButton.addEventListener('click', () => ButtonChooserWeapon(ropeButton))
+pushedDownButton.addEventListener('click', () => ButtonChooserWeapon(pushedDownButton))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
