8535733917c9e47df9266ac544789da5bf76c0e8
Roskapanda69 <markku.pyrro@metropolia.com>
2024-12-11 16:40:46 +0200

    pelaajan nimi menee tietokantaan

--- html/test.js
+++ html/test.js
@@ -36,6 +36,14 @@ async function add_to_narrator_load(list_text){
     const narator_box = document.querySelector('#narrator_text')
     narator_box.innerHTML=`${list_text}`}
 
+async function player_name_save (){
+    const player_name = document.querySelector('#player-id').textContent
+    try{
+        const response = await fetch(`${url_py}/save_name/${player_name}`)
+        if (!response.ok) throw new Error("something went wrong saving name")
+        console.log(response)
+    }catch(error){console.log(error)}}
+
 async function save() {
     let list_note_text = [note_padtext]
     let list_narrtext = [narrtext]
@@ -54,7 +62,7 @@ async function save() {
         console.log('ok')
         } catch (error) {console.log(error.message)}}
 
-async function load() {
+async function load_game() {
     try {
         const response = await fetch(`${url_py}/load`, {})
          if (!response.ok) throw new Error("something went wrong loading")
@@ -62,6 +70,10 @@ async function load() {
         console.log(load_data)
         const nottext = load_data.note_text
         const nartext = load_data.narrator_text
+        const player_name = load_data.player_name
         await add_to_note_padd_load(nottext[0])
         await add_to_narrator_load(nartext[0])
+        console.log(player_name[0])
+        const load_name = document.querySelector('#player-id')
+        load_name.innerText = player_name[0]
     } catch (error) {console.log(error.message)}}
